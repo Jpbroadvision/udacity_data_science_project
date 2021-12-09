8645650235a00c00b18ddd76fc81b0f26e4e56ea
JPbroadvision <jpbroadvision2@gmail.com>
2021-12-09 19:33:30 +0000

    test files

---
+++ explore_us_bikeshare_data/test.py
@@ -0,0 +1,9 @@
+# All available city names
+CITIES_ONLY = ['chicago', 'new york city', 'washington']
+# All available months 
+MONTHS_ONLY = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
+# all available days
+WEEK_DAYS_ONLY = ['all', 'monday', 'tuesday', 'wednesday','thursday', 'friday', 'saturday', 'sunday']
+
+city ="new york city"
+print(city in CITIES_ONLY)
